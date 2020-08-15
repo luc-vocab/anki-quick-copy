2473de6c6ef5c39df64bbd54dd2e15b32b78de83
luc wastiaux <luc.wastiaux@airpost.net>
2020-08-15 15:11:42 +0800

    copy field defined for this notetype into clipboard

--- __init__.py
+++ __init__.py
@@ -3,13 +3,21 @@ import aqt
 import aqt.editor
 import aqt.gui_hooks
 from PyQt5.QtWidgets import QApplication
+# import sys
 
 # using this hook, copy the field we're interested in into the clipboard editor_did_load_note
 
 def editor_loaded_note(editor: aqt.editor.Editor):
-    field_data = editor.note["Chinese"]
-    clipboard = QApplication.clipboard()
-    clipboard.setText(field_data)
+    # get addon configuration
+    config = aqt.mw.addonManager.getConfig(__name__)
+    # check the note type
+    note_type_name = editor.note.model()['name']
+    if note_type_name in config:
+        field_name = config[note_type_name]
+        field_data = editor.note[field_name]
+        # copy to clipboard
+        clipboard = QApplication.clipboard()
+        clipboard.setText(field_data)
 
 
 aqt.gui_hooks.editor_did_load_note.append(editor_loaded_note)
